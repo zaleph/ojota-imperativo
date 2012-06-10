12ecfd9f73b3c611390828553c43de072527ba95
kronleuchter85@gmail.com <kronleuchter85@gmail.com@12c17a7d-0c1e-e816-f2c2-38ec2431413f>
2012-06-10 16:08:40 +0000

    contructor de competencia

--- trunk/tests-main.cpp
+++ trunk/tests-main.cpp
@@ -7,7 +7,9 @@ int main(){
 
     //testAtleta_capacidad();
     //testCompetencia_finalizada();
-    testCompetencia_leDioPositivo();
+    //testCompetencia_leDioPositivo();
+
+    //testCompetencia_sancionarTramposos();
 
     return 0;
 }
