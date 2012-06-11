7e7b5b4d98aa2dffadabd6ee2b034eef035946be
kronleuchter85@gmail.com <kronleuchter85@gmail.com@12c17a7d-0c1e-e816-f2c2-38ec2431413f>
2012-06-11 15:47:19 +0000

    corrigiendo error y jjoo

--- trunk/test-jjoo.h
+++ trunk/test-jjoo.h
@@ -1,8 +1,37 @@
 
 #include "test-competencias.h"
+#include "jjoo.h"
+
+/*
+ * Mocks
+ */
 
 
+ Lista<Lista<Competencia> > mockCronogramas(){
+    Lista<Lista<Competencia> > crons =  Lista<Lista<Competencia> > ();
+    crons.agregar(mockCompetencias());
+    crons.agregar(mockCompetencias());
+    crons.agregar(mockCompetencias());
+    crons.agregar(mockCompetencias());
+    return crons;
+ }
+
+ JJOO mockJJOO(){
+    return JJOO(2012 , mockAtletas() , mockCronogramas());
+ }
+
+
+
+/*
+ * Tests
+ */
 
 void testJJOO_matiSeLaLastra(){
 
 }
+
+void testJJOO_nuevoJJOO(){
+
+}
+
+
